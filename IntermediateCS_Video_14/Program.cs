9eed099fc6dd48db1a5b42ad10e9a6d892007889
ansaris <salman.ansari.usa@gmail.com>
2016-05-25 16:51:04 -0500

    Add class MethodExample.

--- IntermediateCS_Video_14/Program.cs
+++ IntermediateCS_Video_14/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace IntermediateCS_Video_14
+class MethodExample
 {
+    int number;
+
+    // Methods useful for side effects
+    public void SomeMethod(int newValue)
+    {
+        this.number = newValue;
+    }
+
+    public void AnotherMethod()
+    {
+        Console.WriteLine(this.number);
+    }
+
+    // Method useful for value
+    public int Add(int x)
+    {
+        return x + this.number;
+    }
+}
+
+
     class Program
     {
         static void Main(string[] args)
         {
         }
     }
-}
+
